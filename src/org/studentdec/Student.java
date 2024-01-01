2c560fb60a34ea136b20b5af579daba701878c6a
sophiyaguru <sophiyaguru19@gmail.com>
2024-01-01 20:46:49 +0530

    RET123

---
+++ src/org/studentdec/Student.java
@@ -0,0 +1,27 @@
+package org.studentdec;
+//Access Specifier  class className
+public class Student {
+
+	private void studentName() {
+		System.out.println("ajithkumar");
+	}
+	private void studentId() {
+		System.out.println("456");
+	}
+	//main method
+	public static void main(String[] args){
+		
+		//objects
+		
+		Student s=new Student();
+		
+		//method call
+		
+		s.studentName();
+		s.studentId();
+		
+		
+			
+	}
+	
+}
